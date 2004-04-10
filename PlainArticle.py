e7bca87fdae7431873d105fa3a857c635b66a20d
Kit Blake <kitblake@infrae.com>
2004-04-10 07:13:49 +0000

    define order of items in News Publication new select list

--- PlainArticle.py
+++ PlainArticle.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.15.2.1 $
+# $Revision: 1.15.2.2 $
 
 # Zope
 from AccessControl import ClassSecurityInfo
@@ -22,6 +22,7 @@ from Products.SilvaNews.NewsItem import NewsItem, NewsItemVersion
 from Products.Silva import mangle
 
 icon = 'www/news_item.png'
+addable_priority = 3.7
 
 class PlainArticle(NewsItem):
     """A News item that appears as an individual page. By adjusting
