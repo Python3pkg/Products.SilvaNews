eb6ff6142a9126d73d19bca7d27ff56641a693e3
Guido Wesdorp <info@infrae.com>
2004-04-21 14:59:24 +0000

    In the viewers' edit screens the lists of filters are now clickable.

--- AgendaViewer.py
+++ AgendaViewer.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.13.2.1 $
+# $Revision: 1.13.2.2 $
 
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
@@ -55,8 +55,8 @@ class AgendaViewer(NewsViewer):
             for item in parent.objectValues(['Silva Agenda Filter',
                                              'Silva News Filter']):
                 joinedpath = '/'.join(item.getPhysicalPath())
-                pairs.append(('%s (%s)' %
-                              (item.get_title(), joinedpath),
+                pairs.append(('%s (<a href="%s/edit">%s</a>)' %
+                              (item.get_title(), joinedpath, joinedpath),
                               joinedpath))
             if parentpath == ('',):
                 break
