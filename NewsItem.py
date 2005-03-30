0f1a557aa26928279f8573b5cecf44739fdadb98
Guido Wesdorp <info@infrae.com>
2005-03-30 15:48:13 +0000

    Added a new method called 'implements_newsitem' to easily determine whether an object is a news item (required for description check on publication).

--- NewsItem.py
+++ NewsItem.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.23.4.3 $
+# $Revision: 1.23.4.4 $
 
 # Python
 from StringIO import StringIO
@@ -57,6 +57,11 @@ class NewsItem(CatalogedVersionedContent):
         context.f.write('<silva_newsitem id="%s">' % self.id)
         version.to_xml(context)
         context.f.write('</silva_newsitem>')
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                                'implements_newsitem')
+    def implements_newsitem(self):
+        return True
 
 InitializeClass(NewsItem)
 
