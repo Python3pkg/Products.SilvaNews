f4de661bfcac9ef2c8b3b0dcab37aa4ed27b43a0
Antonin Amand <antonin.amand@gmail.com>
2011-04-07 11:12:44 +0200

    better unixtimestamp calc

--- Products/SilvaNews/datetimeutils.py
+++ Products/SilvaNews/datetimeutils.py
@@ -91,12 +91,16 @@ def datetime_with_timezone(dt, tz=local_timezone):
         new_dt = dt.replace(tzinfo=tz)
     return new_dt
 
+
+epoch = datetime(1970, 1, 1, 0, 0, 0, 0, tzinfo=UTC)
+
 def datetime_to_unixtimestamp(dt):
-    """ Workaround a bug in python : unix time is wrong if not in the system
-    timezone
+    """do not handle UTC leap seconds.
     """
-    return int(utc_datetime(dt).astimezone(system_timezone).\
-        strftime("%s"))
+    delta = utc_datetime(dt) - epoch
+    return (delta.microseconds +
+        (delta.seconds + delta.days * 24 * 3600) * 10**6) / 10**6
+
 
 def end_of_day(dt):
     return dt.replace(hour=23, minute=59, second=59)
