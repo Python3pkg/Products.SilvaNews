28d132bb7e92797bbbb202f7e3c2bee79ef8d01d
Andy Altepeter <aaltepet@altepeter.net>
2011-04-12 15:43:17 -0500

    do some factorizing

--- Products/SilvaNews/filters/NewsItemFilter.py
+++ Products/SilvaNews/filters/NewsItemFilter.py
@@ -36,28 +36,61 @@ def brainsorter(a, b):
     btime = b.get_start_datetime
     return cmp(atime, btime)
 
-
-class NewsItemFilter(Filter):
-    """Super-class for news item filters.
-
-    A NewsItemFilter picks up news from news sources. Editors can
-    browse through this news. It can also be used by
-    public pages to expose published news items to end users.
-
-    A super-class for the News Filters (NewsFilter, AgendaFilter)
-    which contains shared code for both filters"""
-
-    grok.implements(INewsItemFilter)
-    grok.baseclass()
+class NewsItemFilterBase(object):
+    """Base class for news item filters.  This is not part of the 
+       silva inheritance story, so that content objects can inherit the
+       meaningful parts of NewsItemFilter and not be grokked as both 
+       Content AND NonPublishable (i.e. AgendaCalendar)
+    """
     security = ClassSecurityInfo()
-
+    
+    _excluded_items = []
     _source_reference_name = 'filter-source'
+    _allowed_source_types = ['Silva News Publication']
+    
+    def _prepare_query ( self, meta_types=None ):
+        """private method preparing the common fields for a catalog query.
 
-    def __init__(self, id):
-        super(NewsItemFilter, self).__init__(id)
-        self._keep_to_path = 0
-        self._excluded_items = []
+        Return: dict holding the query parameters
+        """
+        self.verify_excluded_items()
+        query = {}
+        query['path'] = map(lambda s: "/".join(s), self._get_sources_path())
+        query['version_status'] = 'public'
+        service = getUtility(IServiceNews)
+        query['idx_subjects'] = {
+            'query': self._collect_subjects(service),
+            'operator': 'or'}
+        query['idx_target_audiences'] = {
+            'query': self._collect_target_audiences(service),
+            'operator': 'or'}
+        if not meta_types:
+            meta_types = self.get_allowed_meta_types()
+        query['meta_type'] = meta_types
+        # Workaround for ProxyIndex bug
+        query['sort_on'] = 'idx_display_datetime'
+        query['sort_order'] = 'descending'
+        return query
 
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'excluded_items')
+    def excluded_items(self):
+        """Returns a list of object-paths of all excluded items
+        """
+        self.verify_excluded_items()
+        return self._excluded_items
+
+    def verify_excluded_items(self):
+        do_reindex = 0
+        for item in self._excluded_items:
+            result = self._query(object_path=[item])
+            if not str(item) in [str(i.object_path) for i in result]:
+                self._excluded_items.remove(item)
+                do_reindex = 1
+        if do_reindex:
+            self._p_changed = 1
+            ICataloging(self).reindex()
+            
     # ACCESSORS
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
                               'get_all_sources')
@@ -96,15 +129,6 @@ class NewsItemFilter(Filter):
     def _get_sources_path(self):
         return map(lambda s: s.getPhysicalPath(), self.get_sources())
 
-    security.declareProtected(SilvaPermissions.AccessContentsInformation,
-                              'keep_to_path')
-    def keep_to_path(self):
-        """Returns true if the item should keep to path
-        """
-        return self._keep_to_path
-
-    # MANIPULATORS
-
     security.declareProtected(SilvaPermissions.ChangeSilvaContent,
                               'set_sources')
     def set_sources(self, sources):
@@ -133,24 +157,55 @@ class NewsItemFilter(Filter):
                 self._p_changed = 1
                 self._excluded_items.remove(objectpath)
 
-    security.declareProtected(SilvaPermissions.AccessContentsInformation,
-                              'excluded_items')
-    def excluded_items(self):
-        """Returns a list of object-paths of all excluded items
-        """
-        self.verify_excluded_items()
-        return self._excluded_items
+    def _collect_subjects(self, service):
+        result = set()
+        for sub in self._subjects:
+            node = service._subjects.find(sub)
+            if node is not None:
+                result = result.union(set(node.get_subtree_ids()))
+        return list(result)
 
-    def verify_excluded_items(self):
-        do_reindex = 0
-        for item in self._excluded_items:
-            result = self._query(object_path=[item])
-            if not str(item) in [str(i.object_path) for i in result]:
-                self._excluded_items.remove(item)
-                do_reindex = 1
-        if do_reindex:
-            self._p_changed = 1
-            ICataloging(self).reindex()
+    def _collect_target_audiences(self, service):
+        result = set()
+        for sub in self._target_audiences:
+            node = service._target_audiences.find(sub)
+            if node is not None:
+                result = result.union(set(node.get_subtree_ids()))
+        return list(result)
+    
+    def _query(self, **kw):
+        logger.info('query %s', repr(kw))
+        return self.service_catalog(kw)
+
+InitializeClass(NewsItemFilterBase)
+
+class NewsItemFilter(Filter, NewsItemFilterBase):
+    """Super-class for news item filters.
+
+    A NewsItemFilter picks up news from news sources. Editors can
+    browse through this news. It can also be used by
+    public pages to expose published news items to end users.
+
+    A super-class for the News Filters (NewsFilter, AgendaFilter)
+    which contains shared code for both filters"""
+
+    grok.implements(INewsItemFilter)
+    grok.baseclass()
+    security = ClassSecurityInfo()
+
+
+    def __init__(self, id):
+        super(NewsItemFilter, self).__init__(id)
+        self._keep_to_path = 0
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'keep_to_path')
+    def keep_to_path(self):
+        """Returns true if the item should keep to path
+        """
+        return self._keep_to_path
+
+    # MANIPULATORS
 
     security.declareProtected(SilvaPermissions.ChangeSilvaContent,
                               'set_keep_to_path')
@@ -189,50 +244,6 @@ class NewsItemFilter(Filter):
         return result
 
     # HELPERS
-
-    def _collect_subjects(self, service):
-        result = set()
-        for sub in self._subjects:
-            node = service._subjects.find(sub)
-            if node is not None:
-                result = result.union(set(node.get_subtree_ids()))
-        return list(result)
-
-    def _collect_target_audiences(self, service):
-        result = set()
-        for sub in self._target_audiences:
-            node = service._target_audiences.find(sub)
-            if node is not None:
-                result = result.union(set(node.get_subtree_ids()))
-        return list(result)
-
-    def _prepare_query ( self, meta_types=None ):
-        """private method preparing the common fields for a catalog query.
-
-        Return: dict holding the query parameters
-        """
-        self.verify_excluded_items()
-        query = {}
-        query['path'] = map(lambda s: "/".join(s), self._get_sources_path())
-        query['version_status'] = 'public'
-        service = getUtility(IServiceNews)
-        query['idx_subjects'] = {
-            'query': self._collect_subjects(service),
-            'operator': 'or'}
-        query['idx_target_audiences'] = {
-            'query': self._collect_target_audiences(service),
-            'operator': 'or'}
-        if not meta_types:
-            meta_types = self.get_allowed_meta_types()
-        query['meta_type'] = meta_types
-        # Workaround for ProxyIndex bug
-        query['sort_on'] = 'idx_display_datetime'
-        query['sort_order'] = 'descending'
-        return query
-
-    def _query(self, **kw):
-        logger.info('query %s', repr(kw))
-        return self.service_catalog(kw)
 
     def _query_items(self, **kw):
         brains = self._query(**kw)
