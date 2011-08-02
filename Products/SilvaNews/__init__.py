65d64d9e22a5038d5c8c51063a7a9ffb91a477d7
Sylvain Viollon <thefunny@gmail.com>
2011-08-02 10:32:44 +0200

    Add missing rewrites.

--- Products/SilvaNews/__init__.py
+++ Products/SilvaNews/__init__.py
@@ -13,36 +13,45 @@ silvaconf.extension_depends(["SilvaDocument", "SilvaExternalSources"])
 
 # Specify import path for old classes (for upgrade)
 CLASS_CHANGES = {
-        'Products.SilvaNews.indexing IntegerRangesIndex':
-            'silva.app.news.indexing IntegerRangesIndex',
+    'Products.SilvaNews.PlainAgendaItem PlainAgendaItem':
+        'Products.SilvaNews.AgendaItem AgendaItem',
+    'Products.SilvaNews.PlainAgendaItem PlainAgendaItemVersion':
+        'Products.SilvaNews.AgendaItem AgendaItemVersion',
+    'Products.SilvaNews.PlainArticle PlainArticle':
+        'Products.SilvaNews.NewsItem NewsItem',
+    'Products.SilvaNews.PlainArticle PlainArticleVersion':
+        'Products.SilvaNews.NewsItem NewsItemVersion',
 
-        'Products.SilvaNews.InlineViewer.InlineViewer InlineViewer':
-            'Products.SilvaExternalSources.CodeSource CodeSource',
+    'Products.SilvaNews.indexing IntegerRangesIndex':
+        'silva.app.news.indexing IntegerRangesIndex',
 
-        'Products.SilvaNews.viewers.RSSAggregator RSSAggregator':
-            'silva.app.news.viewers.RSSAggregator RSSAggregator',
-        'Products.SilvaNews.viewers.NewsViewer NewsViewer':
-            'silva.app.news.viewers.NewsViewer NewsViewer',
-        'Products.SilvaNews.viewers.AgendaViewer AgendaViewer':
-            'silva.app.news.viewers.AgendaViewer AgendaViewer',
+    'Products.SilvaNews.InlineViewer.InlineViewer InlineViewer':
+        'Products.SilvaExternalSources.CodeSource CodeSource',
 
-        'Products.SilvaNews.NewsPublication NewsPublication':
-            'silva.app.news.NewsPublication NewsPublication',
+    'Products.SilvaNews.viewers.RSSAggregator RSSAggregator':
+        'silva.app.news.viewers.RSSAggregator RSSAggregator',
+    'Products.SilvaNews.viewers.NewsViewer NewsViewer':
+        'silva.app.news.viewers.NewsViewer NewsViewer',
+    'Products.SilvaNews.viewers.AgendaViewer AgendaViewer':
+        'silva.app.news.viewers.AgendaViewer AgendaViewer',
 
-        'Products.SilvaNews.ServiceNews ServiceNews':
-            'silva.app.news.ServiceNews ServiceNews',
-        'Products.SilvaNews.interfaces IServiceNews':
-            'silva.app.news.interfaces IServiceNews',
-        'Products.SilvaNews.Tree Root':
-            'silva.app.news.Tree Root',
-        'Products.SilvaNews.Tree Node':
-            'silva.app.news.Tree Node',
+    'Products.SilvaNews.NewsPublication NewsPublication':
+        'silva.app.news.NewsPublication NewsPublication',
 
-        'Products.SilvaNews.filters.AgendaFilter AgendaFilter':
-            'silva.app.news.filters.AgendaFilter AgendaFilter',
-        'Products.SilvaNews.filters.NewsFilter NewsFilter':
-            'silva.app.news.filters.NewsFilter NewsFilter',
-        'Products.SilvaNews.filters.CategoryFilter CategoryFilter':
-            'silva.app.news.filters.CategoryFilter CategoryFilter',
+    'Products.SilvaNews.ServiceNews ServiceNews':
+        'silva.app.news.ServiceNews ServiceNews',
+    'Products.SilvaNews.interfaces IServiceNews':
+        'silva.app.news.interfaces IServiceNews',
+    'Products.SilvaNews.Tree Root':
+        'silva.app.news.Tree Root',
+    'Products.SilvaNews.Tree Node':
+        'silva.app.news.Tree Node',
+
+    'Products.SilvaNews.filters.AgendaFilter AgendaFilter':
+        'silva.app.news.filters.AgendaFilter AgendaFilter',
+    'Products.SilvaNews.filters.NewsFilter NewsFilter':
+        'silva.app.news.filters.NewsFilter NewsFilter',
+    'Products.SilvaNews.filters.CategoryFilter CategoryFilter':
+        'silva.app.news.filters.CategoryFilter CategoryFilter',
     }
 
