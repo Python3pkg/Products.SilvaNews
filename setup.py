b39c9ee286680e1649f5b5d28059fef44412bec0
Sylvain Viollon <thefunny@gmail.com>
2012-05-10 11:33:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.9.5'
+version = '2.9.6dev'
 
 def product_readme(filename):
     return  open(os.path.join('Products', 'SilvaNews', filename)).read()
